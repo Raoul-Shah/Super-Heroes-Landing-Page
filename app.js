02ecd4d1570931662c732cf551b44d3e59c00d26
Raoul Shah <74040700+Raoul-Shah@users.noreply.github.com>
2021-05-31 21:21:36 +0200

    Added Scroll-Top (rocket)

--- app.js
+++ app.js
@@ -10,6 +10,45 @@ $(document).ready(function() {
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
 
+        if($(window).scrollTop() > 0) {
+            $('.scroll-top').show();
+        }else {
+            $('.scroll-top').hide();
+        }
+
+        //scroll spy 
+
+     $('section').each(function(){
+
+        let height = $(this).height();
+        let offset = $(this).offset().top - 200;
+        let id = $(this).attr('id');
+        let top = $(window).scrollTop();
+
+        if(top > offset && top < offset + height){
+            $('.navbar ul li a').removeClass('active')
+            $('.navbar').find(`[href="#${id}"]`).addClass('active');
+        }
+
+     });   
+
+    });
+
+    //smooth scrolling 
+
+    $('a[href*="#"]').on('click', function(e){
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        },
+            500,
+            'linear'
+        )
+
     });
 
 });
